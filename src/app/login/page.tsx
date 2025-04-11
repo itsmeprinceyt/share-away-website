661f821f8b4dd1828a1f865c4b36d0e073749619
itsmeprinceyt <mohammadbadshah92@gmail.com>
2025-04-12 00:04:04 +0530

    1.0.0 | Working on auth

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,7 @@ export default function Login(){
     return(
         <div className="h-screen flex flex-col justify-center items-center">    
         <div className="bg-blue-500">
-            <inputi/>
+            <input/>
         </div>
         </div>
     )
