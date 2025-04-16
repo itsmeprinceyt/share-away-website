a332dd18c9472bcfdcbbb5ebcd13e057fcfffbd1
itsmeprinceyt <mohammadbadshah92@gmail.com>
2025-04-17 03:58:30 +0530

    1.12.10 | '/' front-end done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,85 @@
 "use client";
 import Link from 'next/link';
+import Image from 'next/image';
 import useRedirectToHome from '../hooks/useRedirectToHome';
 
 export default function Home() {
   useRedirectToHome();
   return (
-    <div className="h-screen flex flex-col justify-center items-center gap-2">
-      Hero Section
-      <Link href="/login" className="text-lg font-semibold animate-pulse bg-black text-white p-2 px-4 rounded-lg">
-        Get Started
-      </Link>
+    <div className="
+    min-h-screen bg-gradient-to-b from-pink-50 via-pink-200 to-pink-100
+    flex justify-center items-center gap-2">
+
+      <div className="
+      flex flex-col min-[800px]:flex-row items-start gap-10 m-10">
+
+        <div className="
+        max-[920px]:w-[350px] max-[350px]:w-[250px]">
+          <Image
+            className="
+          rounded-4xl border border-pink-300 shadow-pink-500/20 shadow-xl
+          hover:scale-105 hover:shadow-pink-500/40 transition-all duration-500"
+            src={'/art/art8.jpg'}
+            loading='lazy'
+            width={400}
+            height={500}
+            alt="Hero Section Image"
+          />
+        </div>
+
+
+        <div className="
+        flex flex-col justify-center  items-center gap-2 ">
+          <div className="
+          max-[920px]:w-[350px] max-[350px]:w-[250px]">
+            <Image
+              className="
+            rounded-lg border border-pink-300 shadow-pink-500/20 shadow-xl
+            hover:scale-105 hover:shadow-pink-500/40 transition-all duration-500"
+              src={'/art/banner/banner1-text.png'}
+              loading='lazy'
+              width={400}
+              height={200}
+              alt="Logo"
+            />
+          </div>
+
+          <div
+            className="
+          -tracking-tighter max-[920px]:leading-5 max-[800px]:leading-7 leading-7
+          max-[350px]:w-[250px] max-[400px]:text-center max-[920px]:w-[350px]
+          max-[920px]:text-center w-[400px] text-sm p-2 text-pink-950 ">
+
+            ShareAway is a safe, open space where anyone can express their thoughts, feelings, and stories without fear of judgment.
+
+            <br /><br /> Whether you&apos;re going through something tough, celebrating a small win, or just need to get something off your chest, just&nbsp;
+
+            <Link
+              href="/login"
+              className="
+              inline-block bg-pink-600/20 rounded-md px-2 text-pink-600
+              border border-pink-300 hover:border-pink-400 shadow-xl
+              shadow-pink-500/20 hover:scale-105 hover:shadow-pink-500/40
+              transition-all duration-500 animate-pulse hover:animate-none">
+              Share Away 🩷...
+            </Link>
+
+            <div className="
+            inline-block animate-[slide-left-bounce_1s_ease-in-out_infinite]
+            text-xl text-pink-600">
+              &nbsp;&nbsp;&nbsp;←
+            </div>
+
+            <br /><br />
+
+            It&apos;s a community built on empathy, understanding, and support — because everyone deserves to be heard.
+          </div>
+
+        </div>
+
+
+      </div>
+
     </div>
   );
 }
