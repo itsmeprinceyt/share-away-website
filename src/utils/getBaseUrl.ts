9c11d053cd2cd40d660ab2763689a659d94333f7
itsmeprinceyt <mohammadbadshah92@gmail.com>
2025-04-17 06:11:35 +0530

    1.12.11 | fixed - added switch case to handle different production

--- src/utils/getBaseUrl.ts
+++ src/utils/getBaseUrl.ts
@@ -2,11 +2,14 @@
  * @brief Switches between local and production API URLs based on the environment variable.
  */
 const getBaseUrl = () => {
-    if (process.env.NEXT_PUBLIC_ENV === 'dev') {
-        return process.env.NEXT_PUBLIC_LOCAL_API_URL;
-    } else {
-        return process.env.NEXT_PUBLIC_API_URL;
+    switch (process.env.NEXT_PUBLIC_ENV) {
+        case 'dev':
+            return process.env.NEXT_PUBLIC_LOCAL_API_URL;
+        case 'prod':
+            return process.env.NEXT_PUBLIC_API_URL;
+        default:
+            console.warn('Unknown environment: Falling back to production API');
+            return process.env.NEXT_PUBLIC_API_URL;
     }
 };
-
 export default getBaseUrl;
